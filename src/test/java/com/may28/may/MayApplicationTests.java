de8d86c809023975f8df24ad74d85240eb5a7a61
nporeddy <nandaporeddy002@gmail.com>
2025-05-28 12:40:11 -0500

    first commit

---
+++ src/test/java/com/may28/may/MayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.may28.may;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
